0598567e0fbfd590535aa48da1fd225aa8e8a907
Alex Grozav <alex@grozav.com>
2016-08-10 00:22:32 +0300

    Updated email sending.

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,26 @@
 <?php
 
-return function () {
-    echo 'Hello, world!';
+use Flarum\Event\PostWillBeSaved;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Mail\Mailer;
+use Illuminate\Mail\Message;
+
+return function (Dispatcher $events, Mailer $mailer) {
+  $events->listen(DiscussionWillBeSaved::class, function (DiscussionWillBeSaved $event) {
+    $title = $event->discussion->title;
+    $url = $event->discussion->url;
+    $content =  $event->actor->username .
+                " has started a new discussion at " .
+                $link .
+                ": <br>" .
+                $event->discussion->content;
+
+    $mailer->raw(
+        ['raw' => $content],
+        function (Message $message) use ($title) {
+            $message->to('alex@pixevil.com', "Alex Grozav")
+                    ->subject("[Support] " . $title);
+        }
+    );
+  });
 };
