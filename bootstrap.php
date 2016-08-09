6b3b26afedcb0085382e44d783af97518477bf26
Alex Grozav <alex@grozav.com>
2016-08-10 01:21:34 +0300

    Updated email design.

--- bootstrap.php
+++ bootstrap.php
@@ -10,10 +10,15 @@ use Illuminate\Mail\Message;
 return function (Dispatcher $events, Mailer $mailer) {
   $events->listen(DiscussionWasStarted::class, function (DiscussionWasStarted $event) use ($mailer) {
     $discussion = $event->discussion;
-    $content = $discussion->content;
+    $content =  "<h2>There's a new post on the " .
+                "<a href='http://support.pixevil.com'>Pixevil Support Forum</a>" .
+                ".</h2>" .
+                "<br/><br/>" .
+                $discussion->startPost()->content;
+
     $mailer->raw($content, function (Message $message) use ($discussion) {
         $message->to('alex@grozav.com');
-        $message->subject("[Support] " . $discussion->title);
+        $message->subject("[Support Forum] " . $discussion->title);
       }
     );
   });
